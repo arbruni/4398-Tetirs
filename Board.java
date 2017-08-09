400eb9f8c1f0ecfc62ea0c3405140970c6c8204c
arbruni <arb101@txstate.edu>
2017-08-09 09:23:50 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,6 +1,6 @@
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -22,10 +22,17 @@ class Board extends JPanel
 	public static final int HOLD_Y = 64;
 	public static final int UP_NEXT_X = 424;
 	public static final int UP_NEXT_Y = 224;
+	private static final int FONT_SIZE = 20;
+	private static final int SCORE_X = 410;
+	private static final int SCORE_Y = 423;
+	
+	
 	
 	private BufferedImage tiles;
+	private BufferedImage background;
 	private int[][] rotation;
 	private int[][] board;
+	private int[][] colors;
 	private Tetris t;
 	private Tetris holdBlock;
 	private int row; 
@@ -34,6 +41,7 @@ class Board extends JPanel
 	private boolean firstHold;
 	private boolean printHold;
 	private boolean collision;
+	private ScoreTracker score;
 	
 	private Queue<Tetris> blockQueue = new LinkedList<Tetris>();
 
@@ -44,40 +52,29 @@ class Board extends JPanel
 	{ 
 		numRotations = 0;
 		board = new int[20][10];
+		colors = new int[20][10];
 		firstHold = true;
+		score = new ScoreTracker();
 		
+		new MainActivity(this);
 		
+		this.setFocusable(true);
+		this.requestFocusInWindow();
+	
 		newShape();
 		setImage();
+		
+		
 	} 
-	   
-	// setters and getters
-	public int getRow() 
-	{   
-		return row;  
-	}    
-	
-	public void setRow(int row) 
-	{   
-		this.row = row;  
-	}    
-	
-	public int getCol() 
-	{   
-		return col;  
-	} 
-	
-	public void setCol(int col) 
-	{   
-		this.col = col;  
-	}       
-	
+
 	public void setImage() 
 	{   
 		tiles = null;
+		background = null;
 		// reads the image file of the blocks
 		try {
-			tiles = ImageIO.read(new File("C:\\Users\\Amy\\Documents\\GitHub\\Tetris\\tiles.png"));
+			tiles = ImageIO.read(Board.class.getResource("/tiles.png"));
+			background = ImageIO.read(Board.class.getResource("/bg.png"));
 		} catch (IOException e) {
 			
 		} 
@@ -85,20 +82,18 @@ class Board extends JPanel
 
 	// motion control methods
 	public void rotate(){
-		// get the rotation pattern
+		
 		rotation = t.getState(numRotations%t.states.length);
 		numRotations++;
-		
-		// if after rotating, the block goes past the right border, move the block left
+	
 		if(col + rotation[0].length * BLOCK_SIZE > RIGHT_BORDER){
 			col = col - BLOCK_SIZE;
 		}
-		// if after rotating, the block goes past the bottom border move it up
-		// the I shaped block has an error still with this logic, it can go past
-		// the bottom border, if you rotate after it hits. Not sure how to fix that.
+		
 		if(row + rotation.length * BLOCK_SIZE > BOTTOM){
 			row = row - BLOCK_SIZE;
 		}
+
 	}
 	
 	public void speedDown(){
@@ -110,9 +105,31 @@ class Board extends JPanel
 	}
 	
 	public void drop(){
-		row = BOTTOM - rotation.length * BLOCK_SIZE;
+		row = heightOfDropped(col);
 	}
 	
+	private int heightOfDropped(int x) {
+		int height = 0;
+		int width = 0;
+		
+		for(int i = 0; i < rotation.length; i++){
+			for(int j = 0; j < rotation[i][j]; j++){
+				width = j;
+			}
+		}
+		
+		for(int i = 0; i < board.length; i++){
+			for(int j = x; j < rotation[width].length; j++){
+					if(board[i][j] == 1){
+						height = i;
+						System.out.println(height);
+						return height;
+					}
+			}
+		}
+		return height;
+	}
+
 	public void moveRight(){
 		// check to make sure the block doesn't go past the right border
 		if(!(col + 1 + rotation[0].length * BLOCK_SIZE > RIGHT_BORDER))
@@ -126,14 +143,14 @@ class Board extends JPanel
 	}
 	
 	public void hold(){
-		// if you've never called hold before put current block
-		// into hold
+		
 		if(firstHold){
 			firstHold = false;
 			holdBlock = t;
+			t = blockQueue.remove();
+			rotation = t.getShape();
 		}
-		// else if you have called hold before, swap current block
-		// with hold block
+		
 		else{
 			Tetris tempBlock;
 			
@@ -147,36 +164,51 @@ class Board extends JPanel
 		repaint();
 	}
 	
+	
+	private void lineClear(){
+		int height = board.length - 1;
+		
+		for(int i = height; i > 0; i--){
+			int count = 0; 
+			for(int j = 0; j < board[0].length; j++){
+				if(board[i][j] != 0)
+					count++;
+				
+				board[height][j] = board[i][j];
+			}
+			if(count < board[0].length)
+				height--;
+				
+		}
+	}
+	
 	// resets the y position of the block so it stays on the screen
 	public void updatePosition(){
 		
-		if(collision){
-			// takes the location of the block relative to 10X20 board
-			// and prints it to the board[][] array
-			for(int y = 0; y < rotation.length; y++)
-				for(int x = 0; x < rotation[y].length; x++)
-					if(rotation[y][x] != 0)
-						board[(row - TOP)/BLOCK_SIZE + y][(col - LEFT_BORDER)/BLOCK_SIZE + x] = 1;
-					
-
-			newShape();
-		}
-		
-		//Checks if the block has hit the bottom -- would like to instead use
-		//if(isValidAndEmpty(col, row) to check if it his the bottom
-		// and also if it hits a printed block
-		if(!(row + rotation.length * BLOCK_SIZE > BOTTOM)){
+		if(!(row + rotation.length * BLOCK_SIZE > BOTTOM) && isEmptyDown()){
 			row++;
 		}
-		// uncomment to test  isValidAndEmpty()
-		//if(isValidAndEmpty(col, row)){
-		//	row++;
-		//}
 		else{
 			collision = true;
 		}
+		
+		if(collision){
+			
+			score.updateScore();
+			
+			for(int y = 0; y < rotation.length; y++)
+				for(int x = 0; x < rotation[y].length; x++)
+					if(rotation[y][x] != 0){
+						board[(row - TOP)/BLOCK_SIZE + y][(col - LEFT_BORDER)/BLOCK_SIZE + x] = 1;
+						colors[(row - TOP)/BLOCK_SIZE + y][(col - LEFT_BORDER)/BLOCK_SIZE + x] = t.getColor();
+					}
+			
+			lineClear();
+			newShape();
+		}
 
 	}
+	
 	
 	
 	// gets a random block shape, color, and coordinates
@@ -194,45 +226,44 @@ class Board extends JPanel
 		collision = false;
 	}
 	
+	
+	
 	// Printing methods
 	// paints the board and renders the block shapes at the same time
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		
+		g.drawImage(background,0,0,null);
 		printShape(col, row, g, t.getColor(), rotation);
 		printShape(UP_NEXT_X, UP_NEXT_Y, g, blockQueue.peek().getColor(), blockQueue.peek().getShape());
-		paintBoard(g);
-		paintDroppedBlocks(g, t.getColor());
+		
+		paintDroppedBlocks(g);
 			
 		if(printHold){
 			printShape(HOLD_X, HOLD_Y, g, holdBlock.getColor(), holdBlock.getShape());
 		}
 		
+		g.setFont(new Font ("courierNew", Font.BOLD, FONT_SIZE));
+		g.drawString(String.valueOf(score.getScore()), SCORE_X, SCORE_Y);
+		
 	}
+
 	
 	// runs through a 10X20 2D array, everywhere there is a 1, a tile is printed
-	public void paintDroppedBlocks(Graphics g, int color){
+	public void paintDroppedBlocks(Graphics g){
+		int color;
+		
 		for(int y = 0; y < board.length; y++)
 			for(int x = 0; x < board[y].length; x++)
 				if(isOccupied(y,x)){
+					color = colors[y][x];
 					g.drawImage(tiles.getSubimage(color,0,BLOCK_SIZE,BLOCK_SIZE), x * BLOCK_SIZE + LEFT_BORDER,
 						    y * BLOCK_SIZE + TOP, null);
 				}
 
 	}
 	
-	//paints the simple lines of the gameplay board
-	private void paintBoard(Graphics g){
-		// prints the board lines
-		g.drawRect(100, 30, 240, 480);
-		
-		g.drawString("Hold", 420, 40);
-		g.drawRect(400, 50, 144, 120);
-		
-		g.drawString("Up Next", 420, 190);
-		g.drawRect(400, 200, 144, 120);
-	}
+	
 	
 	
 	// prints the tetris blocks based on 2D array coordinates
@@ -256,44 +287,80 @@ class Board extends JPanel
 	//You can for example use a 2D array with Boolean or integer values that represent presence of a square on a location: int[][] squares = new int[20][10];
 
 	//The following method finds the collision
-	public boolean isValidAndEmpty(int x, int y)
+	public boolean isValid()
 	{
-
-	      // //Ensure the piece is in a valid column.
-	      // if(x < -type.getLeftInset(rotation) ||
-	      //     x + type.getDimension() - type.getRightInset(rotation) >= COL_COUNT)
-	      // {
-	      //     return false;
-	      // }
-        
 	       //Ensure the piece is in a valid row.
-	       if(!(row + rotation.length * BLOCK_SIZE > BOTTOM))
-	       {
-	           return true;
-	       }
-
+		//if(!(row + rotation.length * BLOCK_SIZE > BOTTOM))
+		//    return true;
+        //
+	    return false;
+	}
+	
+	public boolean isEmptyLeft(){
+		
+		for(int y = 0; y < rotation.length; y++)
+			for(int x = 0; x < rotation[y].length; x++)
+				if(rotation[y][x] != 0)
+					if((col - LEFT_BORDER)/BLOCK_SIZE + x < 10 && 
+						board[(row - TOP)/BLOCK_SIZE + y][(col - LEFT_BORDER)/BLOCK_SIZE + x + 1] != 0){
+						return false;
+					}
+		
+	    return true;
+			
+	}
+	
+public boolean isEmptyRight(){
+		
+		for(int y = 0; y < rotation.length; y++)
+			for(int x = 0; x < rotation[y].length; x++)
+				if(rotation[y][x] != 0)
+					if((col - LEFT_BORDER)/BLOCK_SIZE + x > LEFT_BORDER/BLOCK_SIZE + 1 && 
+						board[(row - TOP)/BLOCK_SIZE + y][(col - LEFT_BORDER)/BLOCK_SIZE + x - 1] != 0){
+						return false;
+					}
+		
+	    return true;
+			
+	}
+	
+	public boolean isEmptyDown(){
 
 	         /*
 	         * Loop through every tile in the piece and see if it conflicts
 	         * with an existing tile.
 	         * Note: It's fine to do this even though it allows for wrapping * because we've already
 	         * checked to make sure the piece is in a valid location.*/
-	        for(int col = 0; col < rotation.length; col++){
-	            for(int row = 0; row < rotation[col].length; row++){
-	            	if((rotation[col][row] != 0) && isOccupied(x + col, y + row)){
-	            		return false;
-	            	}
-	            }
-
-	        }
+		for(int y = 0; y < rotation.length; y++)
+			for(int x = 0; x < rotation[y].length; x++)
+				if(rotation[y][x] != 0)
+					if((row - TOP)/BLOCK_SIZE + y + 1 < 20 && 
+						board[(row - TOP)/BLOCK_SIZE + y + 1][(col - LEFT_BORDER)/BLOCK_SIZE + x] != 0){
+						return false;
+					}
 	        return true;
 	        
 	}
-
-	private boolean isOccupied(int i, int j) {
-			if(board[i][j] == 1)
-				return true;
-			else
-				return false;
-	}
 	
+	private boolean isOccupied(int y, int x) {
+		if(board[y][x] != 0)
+			return true;
+		else
+			return false;
+	}
+
+	
+	// for testing only
+	private void printBoard(){
+		for(int y = 0; y < board.length; y++){
+			System.out.print("\n");
+			for(int x = 0; x < board[y].length; x++){
+				System.out.print(board[y][x] + " ");
+			}
+		}
+		
+		System.out.println("\n\n\n\n");
+				
+	}
+
+}
